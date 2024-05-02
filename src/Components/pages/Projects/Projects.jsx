b7a28e4c271d793390856843ad5189eb3926278f
Agustin Martinez <agustinmartinezyt@gmail.com>
2024-05-02 02:57:58 -0300

    projects

---
+++ src/Components/pages/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ProjectBox from "../../ProjectBox";
+
+
+const Projects = () => {
+  return (
+    <div>
+      <h2 className="projectHeading">
+        My <b>Projects</b>
+      </h2>
+      {/*en projectPhoto{} va la ruta dentro
+      /*<div className="project">
+        <ProjectBox projectPhoto={} projectName="" />
+        <ProjectBox projectPhoto={} projectName="" />
+        <ProjectBox projectPhoto={} projectName="" />
+        <ProjectBox projectPhoto={} projectName="" />
+      </div>*/}
+    </div>
+  );
+};
+
+export default Projects;

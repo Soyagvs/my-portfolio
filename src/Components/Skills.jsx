3107329c7cf98e5b2729e49cf0611f3e6fb31340
Agustin Martinez <agustinmartinezyt@gmail.com>
2024-05-02 02:59:27 -0300

    components

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  FaReact,
+  FaGithub,
+  FaNpm,
+  FaBootstrap,
+  FaHtml5,
+  FaCss3,
+  FaLaravel,
+  FaNodeJs,
+
+
+} from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
+
+const Skills = ({ skill }) => {
+  const icon = {
+    HTML: <FaHtml5 />,
+    CSS: <FaCss3 />,
+    Javascript: <DiJavascript1 />,
+    React: <FaReact />,
+    Tailwind: <SiTailwindcss />,
+    Bootstrap: <FaBootstrap />,
+    Github: <FaGithub />,
+    Npm: <FaNpm />,
+    Laravel: <FaLaravel />,
+    Node: <FaNodeJs />
+  };
+
+  return (
+    <div title={skill} className="SkillBox">
+      {icon[skill]}
+    </div>
+  );
+};
+
+export default Skills;

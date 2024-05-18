71025bbd7aa3b38a7d12749a4b818c72b005bc2e
Soyagvs <agustinmartinezyt@gmail.com>
2024-05-18 17:07:22 -0300

    bug

--- src/Components/pages/Projects/Projects.jsx
+++ src/Components/pages/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProjectBox from '../../common/ProjectsBox/ProjectBox';
 import '../Projects/Projects.css';
 import projectsData from '../../../data/Projects.json'; 
